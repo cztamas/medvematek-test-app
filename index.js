bc33881b06fd14133f26466bcc7e39e08c5eaf83
tczank <tczank@esysadmin-MacBook-Pro-3.local>
2019-01-31 19:48:16 +0100

    fix typo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.env || 4567;
+const port = process.env.PORT || 4567;
 app.use('/', express.static('./static'));
 
 app.listen(port);
